3f7ea504306c3ca397e9569db3cc094ffe8ffdfe
SE151289 <dathtse151289@fpt.edu.vn>
2024-01-30 13:28:43 +0700

    create edit profile and add auction to favorite list function

--- controller/member.controller.js
+++ controller/member.controller.js
@@ -24,4 +24,60 @@ const getMemberByID = async (req, res) => {
   }
 };
 
-module.exports = { getMemberByID };
+const editProfileMemberByID = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { firstName, lastName, phoneNumber } = req.body;
+    const updateMember = await memberModel.findByIdAndUpdate(
+      id,
+      { firstName, lastName, phoneNumber },
+      {
+        new: true,
+      }
+    );
+
+    if (updateMember) {
+      res.status(HTTP.OK).json({
+        success: true,
+        response: updateMember,
+      });
+    } else {
+      res.status(HTTP.BAD_REQUEST).json({
+        success: false,
+        error: EXCEPTIONS.FAIL_TO_UPDATE_ITEM,
+      });
+    }
+  } catch (error) {
+    res.status(HTTP.INTERNAL_SERVER_ERROR).json(error);
+  }
+};
+
+const addAuctionToFavoriteList = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const addFavoriteAuction = await memberModel.findOneAndUpdate(
+      { _id: id },
+      { $push: { favoriteList: req.body } },
+      { new: true }
+    );
+    if (addFavoriteAuction) {
+      res.status(HTTP.OK).json({
+        success: true,
+        response: addFavoriteAuction,
+      });
+    } else {
+      res.status(HTTP.BAD_REQUEST).json({
+        success: false,
+        error: EXCEPTIONS.FAIL_TO_ADD_FAVORITE,
+      });
+    }
+  } catch (error) {
+    res.status(HTTP.INTERNAL_SERVER_ERROR).json(error);
+  }
+};
+
+module.exports = {
+  getMemberByID,
+  editProfileMemberByID,
+  addAuctionToFavoriteList,
+};
--- models/member.model.js
+++ models/member.model.js
@@ -8,6 +8,7 @@ const memberSchema = mongoose.Schema(
     lastName: { type: String, minLength: 2, require: true },
     phoneNumber: { type: String, minLength: 10, require: true },
     idCard: [{ type: String }],
+    favoriteList:[{type: Object}]
   },
   { timestamps: true }
 );
